aecb45b61df326e9fff8b976e3b21ddced965980
Travis Alpers <arbiter34@gmail.com>
2020-06-06 19:41:29 -0700

    - Added QR code scanning from file.

---
+++ qr/src/main/kotlin/com/arbiter34/ovpnconnect/qr/QRDecoder.kt
@@ -0,0 +1,48 @@
+package com.arbiter34.ovpnconnect.qr
+
+import com.arbiter34.ovpn.TOTP.MigrationPayload
+import com.google.zxing.BinaryBitmap
+import com.google.zxing.LuminanceSource
+import com.google.zxing.MultiFormatReader
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource
+import com.google.zxing.common.HybridBinarizer
+import java.io.File
+import java.net.URI
+import java.net.URLDecoder
+import java.util.Base64
+import javax.imageio.ImageIO
+
+object QRDecoder {
+
+    fun decode(
+        filePath: String
+    ): ByteArray {
+        val file = File(filePath)
+
+        if (!file.exists()) {
+            throw IllegalStateException("Unable to find file ${file.absolutePath}")
+        }
+
+        val image = ImageIO.read(file)
+        val luminanceSource = BufferedImageLuminanceSource(image)
+        val bitmap = BinaryBitmap(HybridBinarizer(luminanceSource))
+
+        val text = MultiFormatReader().decode(bitmap).text
+        val uri = URI(text)
+
+
+        val queryMap = mutableMapOf<String, String>()
+        val queries = uri.rawQuery.split("=")
+        for (i in 0 until queries.size / 2) {
+            queryMap[queries[i]] = URLDecoder.decode(queries[i+1])
+        }
+        return MigrationPayload.parseFrom(
+            Base64.getDecoder()
+                .decode(
+                    queryMap["data"]
+                )
+        ).otpParametersList
+            .first()
+            .toByteArray()
+    }
+}
--- src/main/kotlin/com/arbiter34/ovpnconnect/ScanQRCode.kt
+++ src/main/kotlin/com/arbiter34/ovpnconnect/ScanQRCode.kt
@@ -1,18 +1,41 @@
 package com.arbiter34.ovpnconnect
 
+import com.arbiter34.ovpnconnect.qr.QRDecoder
+import com.arbiter34.ovpnconnect.util.AES
 import picocli.CommandLine.Command
 import picocli.CommandLine.Option
+import java.io.File
+import java.io.FileOutputStream
+import java.util.concurrent.Callable
 
 @Command(name = "scanQrCode")
 class ScanQRCode(
-): Runnable {
+): Callable<Int> {
     @Option(names = ["-p", "--password"], description = ["Password"], interactive = true)
     private var password: CharArray = CharArray(0)
 
     @Option(names = ["-u", "--username"], description = ["Your OpenVPN username"], required = true)
     private var username: String = ""
 
-    override fun run() {
-        TODO("Not yet implemented")
+    @Option(names = ["-f", "--file"], description = ["File path to the QR Code Image"])
+    private var qrPath: String = ""
+
+    @Option(names = ["-o", "--output"], description = ["File path to output your encrypted secret to be used with the connect command."])
+    private var outputPath: String = ""
+
+    override fun call(): Int {
+        val secret = QRDecoder.decode(qrPath)
+
+        val encrypted = AES.encrypt(
+            password.map { "$it" }.reduce { acc, s -> "$acc$s" }.toByteArray(Charsets.UTF_8),
+            username.toByteArray(Charsets.UTF_8),
+            secret
+        )
+
+        FileOutputStream(File(outputPath))
+            .use {
+                it.write(encrypted)
+            }
+        return 0
     }
 }
