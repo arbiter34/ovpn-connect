04ddb34a240c2eacdaef514bbfb37e496c7e5b3b
Travis Alpers <arbiter34@gmail.com>
2020-06-07 15:30:35 -0700

    - Beginning of adding GRPC to allow the actions to be driven by a browser extension or a web app.

--- client/src/main/kotlin/com/arbiter34/ovpnconnect/grpc/client/ConnectClient.kt
+++ client/src/main/kotlin/com/arbiter34/ovpnconnect/grpc/client/ConnectClient.kt
@@ -1,5 +1,7 @@
 package com.arbiter34.ovpnconnect.grpc.client
 
+import com.arbiter34.ovpnconnect.proto.Connect.Action
+import com.arbiter34.ovpnconnect.proto.Connect.ConnectionRequest
 import com.arbiter34.ovpnconnect.proto.ConnectionServiceGrpc
 import com.arbiter34.ovpnconnect.proto.ConnectionServiceGrpc.ConnectionServiceFutureStub
 import com.arbiter34.ovpnconnect.proto.ConnectionServiceGrpc.ConnectionServiceStub
@@ -12,8 +14,21 @@ class ConnectClient(
     val channel = channelBuilder.build()
     val client = ConnectionServiceGrpc.newStub(channel)
 
-    fun connect() {
-        client.connection()
+    fun connect(
+        username: String,
+        password: String,
+        configFile: String? = null,
+        config: String? = null
+    ) {
+        TODO("")
+//        if (config == null && configFile == null) {
+//
+//        }
+//        client.connection(
+//            ConnectionRequest.newBuilder()
+//                .setAction(Action.CONNECT)
+//
+//        )
     }
 
 }
